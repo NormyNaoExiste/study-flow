b05c97c36b342182e7d4f0a8928e966ca60992c0
Davi Felipe <felipedaviam@gmail.com>
2026-09-08 15:14:45 -0300

    estou cansado

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const toggleButton = document.getElementById('toggle-btn');
 const sidebar = document.getElementById('sidebar');
 
 const mainPage = document.getElementById('mainpage')
+const cenas = {
+    home: `<div class="container">
+            <h2>Lorem</h2>
+            <p>Paragrafo</p>
+        </div>
+        <div class="container">
+            <h2>Lorem</h2>
+            <p>Paragrafo</p>
+        </div>
+        <div class="container">
+            <h2>Lorem</h2>
+            <p>Paragrafo</p>
+        </div>`,
+    pomodoro: `<div class="container">
+            <h2>Pomodoro</h2>
+            <p>25:00</p>
+        </div>`,
+    materias: `<div class="container">
+            <h2>Materias</h2>
+            <p>Aqui fica as materias</p>
+        </div>`
+}
 
 function toggleSidebar(){
     sidebar.classList.toggle('close')
@@ -23,8 +45,23 @@ function toggleSubMenu(button){
     }
 }
 
-function changeScene(scene){
-    if(scene == 'pomodoro'){
-        mainPage.innerHTML = '<p>teste</p>'
+function changeScene(sceneName){
+    if(cenas[sceneName]){
+        mainPage.innerHTML = cenas[sceneName]
+    } else{
+        alert(`cena ${sceneName} não existe`)
+    }
+}
+
+function toggleActive(clicado){
+    const activeLink = clicado.closest('li')
+    const todosLinks = document.querySelectorAll("li")
+
+    todosLinks.forEach(li => li.classList.remove("active"))
+    activeLink.classList.add("active")
+
+    const parentLink = activeLink.closest("ul.sub-menu")?.closest('li')
+    if(parentLink){
+        parentLink.classList.add("active")
     }
 }
